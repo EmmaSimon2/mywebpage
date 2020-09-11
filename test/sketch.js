8d57bc2fcbf69e891201cc31054bd6dedf15d313
EmmaSimon2 <simonemm@msu.edu>
2020-09-11 14:18:49 -0400

    making howland

--- test/sketch.js
+++ test/sketch.js
@@ -7,6 +7,15 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  stroke(0);
-  line(200,100,10,20);
+  noStroke(0);
+  fill(144,183,192);
+  rect(150,200,200,200);
+  fill(144,183,192);
+  rect(125,350,250,70);
+
+  fill(45);
+  triangle(125,200,250,100,375,200);
+  fill(144,183,192);
+  triangle(125,300,250,225,375,300);
+
 }
