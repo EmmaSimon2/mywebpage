5f38e96bd7042a0271446fb09215812227ab3efb
EmmaSimon2 <simonemm@msu.edu>
2020-09-11 13:17:47 -0400

    a

--- test/sketch.js
+++ test/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   // put setup code here
-  createCanvas(500,500);
+createCanvas(500,500);
 
 }
 
 function draw() {
   // put drawing code here
-  line(10,10,200,200);
+line(10,10,200,200);
 }
