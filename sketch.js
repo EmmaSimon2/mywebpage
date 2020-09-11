2fd5be9632600bd962cc0989a380f1bdc5b52cf3
EmmaSimon2 <simonemm@msu.edu>
2020-09-11 13:31:30 -0400

    is it this one?

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   // put setup code here
 createCanvas(500,500);
-
+background(37,100,74);
 }
 
 function draw() {
   // put drawing code here
-line(10,10,200,200);
+line(100,100,200,200);
 }
