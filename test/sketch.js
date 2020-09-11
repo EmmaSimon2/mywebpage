9059fd26eed1ad1aef050518c907aaf913723dea
EmmaSimon2 <simonemm@msu.edu>
2020-09-11 13:30:34 -0400

    color change line change

--- test/sketch.js
+++ test/sketch.js
@@ -1,10 +1,10 @@
 function setup() {
   // put setup code here
 createCanvas(500,500);
-
+background(100,37,249);
 }
 
 function draw() {
   // put drawing code here
-line(400,100,300,400);
+line(100,100,20,20);
 }
